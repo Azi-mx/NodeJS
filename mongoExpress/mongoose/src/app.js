2847488d3a6eaef0eb3202a631fccc539eb578dd
Azi-mx <bagsariyaa@gmail.com>
2023-08-02 18:15:29 +0530

    updates

---
+++ mongoExpress/mongoose/src/app.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose')
+//Creating connection and create new db
+mongoose.connect("mongodb://localhost:27017/azimdatabase",{useNewUrlParser:true}).then(()=>{
+    console.log("Connection succesfully established")
+}).catch((err)=>console.log(err))
+const playlistSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    age:Number,
+    job:String,
+    date:{
+        type:Date,
+        default:Date.now
+    }
+})
+//Collection creation
+const Playlist = new mongoose.model("Playlist",playlistSchema)
+
+//Creation of a document or insert a document using async await
+//also creation of multiple documents
+const createDocument = async ()=>{
+    try{
+        const firstPlaylist = new Playlist({
+            name:"Rinkal",
+            age:34,
+            job:"Teacher"
+        })
+        const secondPlaylist = new Playlist({
+            name:"Sachi",
+            age:29,
+            job:"Consultant"
+        })
+        const thirdPlaylist = new Playlist({
+            name:"Megha",
+            age:26,
+            job:"Accountant"
+        })
+        const fourthPlaylist = new Playlist({
+            name:"Sanket",
+            age:24,
+            job:"Teacher"
+        })
+        const result = await Playlist.insertMany([secondPlaylist,thirdPlaylist,fourthPlaylist]);
+        console.log(result)
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+// createDocument();
+
+//reading document & 
+const getDocument = async ()=>{
+    try{
+    const result = await Playlist
+    //Comparison operator:
+    // .find({age:24})
+    // .find({age:{$gte:24}})
+    // .find({job:{$in:["Teacher","Consultant"]}})
+    // .find({job:{$nin:["Teacher","Consultant"]}})
+    //logical Operator:
+    // .find({$or:[{job:"Teacher"},{name:"Sachi"}]})
+    // .find({$and:[{job:"Teacher"},{name:"Sanket"}]})
+    // .find({$nor:[{job:"Teacher"},{name:"Sachi"}]})
+    // .find({$not:[{job:"Teacher"},{name:"Rinkal"}]})
+    .find()
+    //To select how many records of the query u wanted to show
+    .select({name:1})
+    //To count documents in a collection
+    // .countDocuments();
+
+    //To get documents in order
+    .sort({name:1});
+    // .limit(1)
+    console.log(result)
+    }catch(err){
+        console.log(err)
+    }
+}
+getDocument();
