9f09011bdd296103ad243111eba2cb6fbdc84741
Vishal-8155 <bagsariyaa@gmail.com>
2023-08-04 12:43:27 +0530

    Updates

---
+++ Task/src/HTN.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+const mainpath = path.join(__dirname, '../Public/css/')
+console.log(mainpath);
+app.use(express.static(mainpath))
+app.set('view engine', 'ejs')
+
+app.get('/Home',(req,res)=>{
+    res.render('index')
+})
+app.get('/AboutUS',(req,res)=>{
+    res.render('about')
+})
+app.get('/Contact',(req,res)=>{
+    res.render('service')
+})
+app.get('/User',(req,res)=>{
+    res.render('form')
+})
+app.listen(8000,()=>{
+    console.log('8000 server is running')
+})
